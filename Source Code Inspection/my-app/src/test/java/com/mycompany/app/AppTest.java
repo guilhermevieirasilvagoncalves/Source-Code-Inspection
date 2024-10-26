852a7eda613d02650e347975166058fcbd626d6f
Kenzo Sugai <kenzosugai@gmail.com>
2024-10-25 22:58:28 -0300

    Testes usando junit

--- Source Code Inspection/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ Source Code Inspection/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -8,24 +8,49 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    
-    
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        //assertTrue( true );
-    }
-
-    @Test
-    public void getvalorCerto(){
+    public void TesteGetQuantidade(){
 
         PapelMoeda papel = new PapelMoeda(10, 5);
 
         int quantidade = papel.getQuantidade();
 
         Assert.assertEquals(5, quantidade);
-
-        //assertTrue(true);
     }
+
+    @Test
+    public void testCriarPapelMoedaValido() {
+        PapelMoeda nota = new PapelMoeda(50, 2); 
+        
+        Assert.assertEquals(50, nota.getValor()); 
+        Assert.assertEquals(2, nota.getQuantidade());
+    }
+
+    // @Test
+    // public void inserirPapelMoeda(){
+
+    //     TicketMachine tm = new TicketMachine(0);
+
+    //     tm.inserir(10);
+
+    //     int saldo = tm.getSaldo();
+
+    //     Assert.
+    // }
+
+    @Test
+    public void testEmitirTicketComSaldoSuficiente() throws SaldoInsuficienteException, PapelMoedaInvalidaException {
+        TicketMachine machine = new TicketMachine(5);
+        machine.inserir(10);
+        machine.imprimir();
+        Assert.assertEquals(10, machine.getSaldo()); // O saldo deve ser atualizado após emitir o ticket
+    }
+
+    @Test(expected = PapelMoedaInvalidaException.class)
+    public void testInserirDinheiroMenorQueZero() throws PapelMoedaInvalidaException {
+        TicketMachine machine = new TicketMachine(5);
+        machine.inserir(-5); // Valor inválido
+    }
+
 }
